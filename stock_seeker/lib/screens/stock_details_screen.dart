d4557bc794759a61ecb0396face8af0539c18d7a
Ricardo Ribeiro <71141428+rnribeiro@users.noreply.github.com>
2024-05-21 22:08:40 +0100

    updating details to be able to show comparison

--- stock_seeker/lib/screens/stock_details_screen.dart
+++ stock_seeker/lib/screens/stock_details_screen.dart
@@ -2,16 +2,58 @@ import 'package:flutter/material.dart';
 import '../data/stock.dart';
 import '../charts/detail_chart.dart';
 
-class StockDetailsScreen extends StatelessWidget {
-  final Stock stock;
+class StockDetailsScreen extends StatefulWidget {
+  final List<Stock> stockList;
 
-  const StockDetailsScreen({Key? key, required this.stock}) : super(key: key);
+  const StockDetailsScreen({super.key, required this.stockList});
+
+  @override
+  _StockDetailsScreenState createState() => _StockDetailsScreenState();
+}
+
+class _StockDetailsScreenState extends State<StockDetailsScreen> {
+  String selectedRange = '1D';
+  late List<StockValue> chartData;
+
+  @override
+  void initState() {
+    super.initState();
+    updateChartData();
+  }
+
+  void updateChartData() {
+    setState(() {
+      switch (selectedRange) {
+        case '1D':
+          chartData = widget.stockList[0].getLastXDaysData(1);
+          break;
+        case '1W':
+          chartData = widget.stockList[0].getLastXDaysData(7);
+          break;
+        case '1M':
+          chartData = widget.stockList[0].getLastXDaysData(30);
+          break;
+        case '1Y':
+          chartData = widget.stockList[0].getLastXDaysData(365);
+          break;
+        case '5Y':
+          chartData = widget.stockList[0].getLastXDaysData(365 * 5);
+          break;
+        default:
+          chartData = widget.stockList[0].getLastXDaysData(1);
+          break;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final isSingleStock = widget.stockList.length == 1;
     return Scaffold(
       appBar: AppBar(
-        title: Text('${stock.name} (${stock.symbol})'),
+        title: Text(isSingleStock
+            ? '${widget.stockList[0].name} (${widget.stockList[0].symbol})'
+            : '${widget.stockList[0].symbol} vs ${widget.stockList[1].symbol}'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
@@ -19,6 +61,8 @@ class StockDetailsScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             _buildStockInfo(),
+            const SizedBox(height: 20),
+            _buildDateRangeButtons(),
             const SizedBox(height: 20),
             _buildPriceChart(),
             const SizedBox(height: 20),
@@ -31,36 +75,149 @@ class StockDetailsScreen extends StatelessWidget {
   }
 
   Widget _buildStockInfo() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          stock.name,
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-        Text(
-          stock.symbol,
-          style: TextStyle(fontSize: 18, color: Colors.lightBlue),
-        ),
-        SizedBox(height: 10),
-        Text(
-          '\$${stock.quote.close}',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-      ],
+    final isSingleStock = widget.stockList.length == 1;
+    return isSingleStock
+        ? Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.stockList[0].name,
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    widget.stockList[0].symbol,
+                    style:
+                        const TextStyle(fontSize: 18, color: Colors.lightBlue),
+                  ),
+                ],
+              ),
+              Text(
+                '\$${widget.stockList[0].quote.close}',
+                style:
+                    const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ],
+          )
+        : Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.stockList[0].name,
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    widget.stockList[0].symbol,
+                    style:
+                        const TextStyle(fontSize: 18, color: Colors.lightBlue),
+                  ),
+                  Text(
+                    '\$${widget.stockList[0].quote.close}',
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.stockList[1].name,
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    widget.stockList[1].symbol,
+                    style:
+                        const TextStyle(fontSize: 18, color: Colors.lightBlue),
+                  ),
+                  Text(
+                    '\$${widget.stockList[1].quote.close}',
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ],
+          );
+  }
+
+  Widget _buildDateRangeButtons() {
+    final ranges = ['1D', '1W', '1M', '1Y', '5Y'];
+
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: ranges.map((range) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor:
+                    selectedRange == range ? Colors.blue : Colors.grey[300],
+              ),
+              onPressed: () {
+                setState(() {
+                  selectedRange = range; // Update selectedRange
+                });
+                updateChartData(); // Call updateChartData
+              },
+              child: Text(range),
+            ),
+          );
+        }).toList(),
+      ),
     );
   }
 
   Widget _buildPriceDetails() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        _buildPriceDetailRow('Open', stock.quote.open),
-        _buildPriceDetailRow('Close', stock.quote.close),
-        _buildPriceDetailRow('High', stock.quote.high),
-        _buildPriceDetailRow('Low', stock.quote.low),
-      ],
-    );
+    final isSingleStock = widget.stockList.length == 1;
+    return isSingleStock
+        ? Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildPriceDetailRow('Open', widget.stockList[0].quote.open),
+              _buildPriceDetailRow('Close', widget.stockList[0].quote.close),
+              _buildPriceDetailRow('High', widget.stockList[0].quote.high),
+              _buildPriceDetailRow('Low', widget.stockList[0].quote.low),
+            ],
+          )
+        : Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const SizedBox(width: 60),
+                  Text(widget.stockList[0].symbol),
+                  Text(widget.stockList[1].symbol),
+                ],
+              ),
+              _buildPriceDetailRowComparison(
+                  'Open',
+                  widget.stockList[0].quote.open,
+                  widget.stockList[1].quote.open),
+              _buildPriceDetailRowComparison(
+                  'Close',
+                  widget.stockList[0].quote.close,
+                  widget.stockList[1].quote.close),
+              _buildPriceDetailRowComparison(
+                  'High',
+                  widget.stockList[0].quote.high,
+                  widget.stockList[1].quote.high),
+              _buildPriceDetailRowComparison('Low',
+                  widget.stockList[0].quote.low, widget.stockList[1].quote.low),
+            ],
+          );
   }
 
   Widget _buildPriceDetailRow(String label, String value) {
@@ -71,11 +228,35 @@ class StockDetailsScreen extends StatelessWidget {
         children: [
           Text(
             label,
-            style: TextStyle(fontSize: 16),
+            style: const TextStyle(fontSize: 16),
           ),
           Text(
-            '\$${value}',
-            style: TextStyle(fontSize: 16),
+            '\$$value',
+            style: const TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPriceDetailRowComparison(
+      String label, String value1, String value2) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            label,
+            style: const TextStyle(fontSize: 16),
+          ),
+          Text(
+            '\$$value1',
+            style: const TextStyle(fontSize: 16),
+          ),
+          Text(
+            '\$$value2',
+            style: const TextStyle(fontSize: 16),
           ),
         ],
       ),
@@ -87,7 +268,7 @@ class StockDetailsScreen extends StatelessWidget {
       child: Container(
         height: 200,
         child: DetailChart(
-          stockList: [stock], // Pass the stock object to DetailChart
+          stockList: widget.stockList, // Passing the stock list
         ),
       ),
     );
